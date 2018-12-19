cc06d2b3628168fc771d08972aef6e0bcdbe23fb
Valer Bogoslovov <valeribogoslovov@hotmail.com>
2018-12-19 15:00:47 +0200

    Initial commit

---
+++ Overbooked.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Overbooked.Web.Models;
+using Overbooked.Data;
+using Overbooked.Data.Models;
+
+[assembly: HostingStartup(typeof(Overbooked.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Overbooked.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OverbookedContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("OverbookedContextConnection")));
+
+                services.AddDefaultIdentity<User>()
+                    .AddEntityFrameworkStores<OverbookedContext>();
+            });
+        }
+    }
+}
